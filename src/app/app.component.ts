8dfcf1927ea53249826974f7041dd998f98e00d3
Pusilkom <adil@pusilkom.com>
2023-07-19 11:33:39 +0700

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  myForm!: FormGroup;
+  result: string = '';
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      value: '',
+    });
+  }
+
+  /**
+   * Create fun() where if the user press enter key, it will console log the value
+   */
+  fun(e: any) {
+    // if e target value is number, then console log "number"
+    // else if e target value is string, then console log "string"
+    // else console log "other"
+
+
+    if (isNumeric(e.target.value)) {
+      this.result = 'number';
+    } else if (typeof e.target.value === 'string' && e.target.value !== '') {
+      this.result = 'string';
+    } else {
+      this.result = 'other';
+    }
+  }
+}
+
+function isNumeric(value: string) {
+  return /^-?\d+$/.test(value);
 }
