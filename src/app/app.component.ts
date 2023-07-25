66cbb9a71ac33ca07b48ec48b6a616c571538b50
Pusilkom <adil@pusilkom.com>
2023-07-25 17:01:42 +0700

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  animations: [
+    trigger('move', [
+      state('forward', style({
+        transform: 'translateX({{forward_value}})'
+      }), {params: {forward_value: '100%'}}),
+      state('backward', style({
+        transform: 'translateX(0)'
+      })),
+      transition('* => forward', [
+        animate('1s'),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  forwardValue = "10%"
+  isOpen = false;
+  counter = 0;
+
+  toggleState() {
+    this.isOpen = !this.isOpen;
+  }
+
+  increment() {
+    this.counter++;
+  }
 }
